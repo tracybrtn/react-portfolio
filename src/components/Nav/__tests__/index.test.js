f3cc40c7ccf7fb96f3e8a7253f142d987fc8fd04
tracybrtn <tracynburton@gmail.com>
2023-10-03 13:13:37 +0200

    passing Nav tests

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,7 +1,28 @@
 //import dependencies
 import React from "react";
 import { render, cleanup } from '@testing-library/react';
+
 import Nav from '..';
 
-//import cleanup function
+//clean up function
 afterEach(cleanup);
+
+const mockCurrentSection = jest.fn();
+const mockSetCurrentSection = jest.fn();
+
+describe('Nav component', () => {
+  //baseline test
+  it('renders', () => {
+    render(<Nav 
+      currentSection={mockCurrentSection}
+      setCurrentSection={mockSetCurrentSection}
+      />);
+  })
+
+  //snapshot test
+  it('matches snapshot', () => {
+    const { asFragment } = render(<Nav />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
